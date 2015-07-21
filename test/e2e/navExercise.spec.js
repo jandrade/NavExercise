ef3a27eacdc4b191b4d4fb694be2dfa1136cadc4
Juan David Andrade <juandavidandrade@gmail.com>
2015-07-21 13:50:59 -0500

    Fixed e2e test

--- test/e2e/navExercise.spec.js
+++ test/e2e/navExercise.spec.js
@@ -10,14 +10,15 @@
 
 	describe('E2E > navExercise:', function() {
 
+		/**
+		 * Imports
+		 */
 		var webdriver = require('selenium-webdriver'),
 			chrome = require('selenium-webdriver/chrome'),
 			path = require('chromedriver').path,
 			driver;
 
-		//jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
-
-
+		
 		beforeEach(function(done) {
 			var service = new chrome.ServiceBuilder(path).build();
 			chrome.setDefaultService(service);
@@ -25,6 +26,9 @@
 			driver = new webdriver.Builder().
 	            withCapabilities(webdriver.Capabilities.chrome()).
 	            build();
+
+			driver.get('http://localhost:3000/');
+	        done();
 		});
 
 		// Close the website after each test
@@ -33,22 +37,35 @@
 		});
 
 		it('Should click on a menu item and display the backdrop', function (done) {
-			driver.get('http://localhost:3000/');
+			
+    		var TITLE = 'HUGE NavExercise',
+    			element = driver.findElement(webdriver.By.tagName('body')),
+    			menuItem = driver.findElement(webdriver.By.css('.navbar > ul > li:nth-child(1) a')),
+    			secondMenuItem = driver.findElement(webdriver.By.css('.navbar > ul > li:nth-child(2) a'));
 
-    		var title,
-    			element = driver.findElement(webdriver.By.tagName('body'));
+    		// get page title
+    		driver.getTitle()
+    			.then(function (title) {
+    				expect(title).toBe(TITLE);
+    				// click on first item of the navigation
+					menuItem.click();	
+					done();
+				});
+    			
+    		// get window url
+			driver.getCurrentUrl().
+        		then(function(url) {
+        			// validates if the location.hash has been changed
+        			expect(/work/.test(url)).toBe(true);
+        			// click on first item of the navigation
+        			secondMenuItem.click();
+        			// checks if the page is displaying the backdrop
+        			element.getAttribute('class').then(function(id) {
+				    	expect(id).toBe('menu-open');
+				        done();
+				    });
+        		});
 
-    		driver.sleep(1000).then(function () {
-			   return driver.getTitle().then(function (title) {
-			       expect(title).toBe('HUGE NavExercise');
-			       done();
-			   });
-			});
-    		/*
-		    element.getAttribute('class').then(function(id) {
-		        expect(id).toBe('menu-open');
-		        done();
-		    });*/
 		});
 	});
 
